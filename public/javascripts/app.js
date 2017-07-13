8829d5aa443638a85a6886186f44c464c0b20a05
Brian Francisco <ibrianfrancisco@gmail.com>
2017-07-12 18:15:38 -0700

    new approach

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,7 @@
 (function() {
 'use strict';
 
-  angular.module('app', ['ui.router', 'ngResource'])
+  angular.module('app', ['ui.router', 'ngResource', 'ngFileUpload'])
     .config(configRoutes);
 
   configRoutes.$inject = ['$stateProvider', '$urlRouterProvider', '$httpProvider'];
--- public/javascripts/controllers/report-controller.js
+++ public/javascripts/controllers/report-controller.js
@@ -5,9 +5,9 @@
     .module('app')
     .controller('ReportController', ReportController);
 
-  ReportController.$inject = ['$state', 'PostService'];
+  ReportController.$inject = ['$scope', '$state', 'Upload', 'PostService', '$timeout'];
 
-  function ReportController($state, PostService) {
+  function ReportController($scope, $state, Upload, PostService, $timeout) {
     let vm = this;
 
     vm.createPost = function(subject, category, description, name, phone, email) {
@@ -20,6 +20,33 @@
         email
       }, data => $state.go('thankyou'));
     }
+
+    $scope.uploadFiles = function(file, errFiles) {
+        $scope.f = file;
+        $scope.errFile = errFiles && errFiles[0];
+        console.log(file);
+        if (file) {
+            file.upload = Upload.upload({
+                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                method: 'POST',
+                data: {file: file}
+            });
+            console.log(file, errFiles);
+
+            file.upload.then(function (response) {
+                $timeout(function () {
+                    file.result = response.data;
+                });
+            }, function (response) {
+                if (response.status > 0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            }, function (evt) {
+                file.progress = Math.min(100, parseInt(100.0 *
+                                         evt.loaded / evt.total));
+            });
+        }
+    }
+
   }
 
 })();
